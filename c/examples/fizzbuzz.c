baac8180ebfc6c023a6773ad3383fbb5b896cc87
Emily <emily@skrivapa.se>
2012-12-16 14:42:11 +0100

    oops

--- c/examples/fizzbuzz.c
+++ c/examples/fizzbuzz.c
@@ -2,7 +2,7 @@
 
 void main () {
     int i;
-    for (int i = 1; i <= 100; i++) {
+    for (i = 1; i <= 100; i++) {
         if (!(i % 15))
             printf("FizzBuzz\n");
         else if (!(i % 3))
